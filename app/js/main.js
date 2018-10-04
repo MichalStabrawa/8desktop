ab92f032364562cc39a693901511f73d1ee4d99f
Michal Stabrawa <mistaaqua@gmail.com>
2018-10-04 21:50:19 +0200

    rwd wrapper-section

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,21 @@
 $(document).ready(function(){
     $('#hamburger').click(function(){
         $('.nav-item2-show').toggle(500);
+    });
+
+    $('.row-item').mouseenter(function(){
+        $(this).find('img').css('transform','scale(1.1)');
+    })
+
+    $('.row-item').mouseleave(function(){
+        $(this).find('img').css('transform','scale(1.0)');
+    })
+
+    $('.row-item11').mouseenter(function(){
+        $(this).find('img').css('transform','scale(1.1)');
+    })
+
+    $('.row-item11').mouseleave(function(){
+        $(this).find('img').css('transform','scale(1.0)');
     })
 })
